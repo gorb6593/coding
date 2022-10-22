d46df8fe3dc524c605e6f1c38cd2d4499fdf3ff1
haekyu <gorb6593@gmail.com>
2022-10-22 11:46:12 +0900

    test1

---
+++ level_1/compare_number.js
@@ -0,0 +1,22 @@
+//정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+
+function solution(num1, num2) {
+    var answer = 0;
+    
+    if(num1 == num2){
+        answer = 1;
+    }else{
+        answer = -1;
+    }
+    
+    return answer;
+}
+
+//다른 사람 풀이
+
+function solution(num1, num2) {
+    return num1===num2?1:-1;
+}
+
+
+const solution = (num1, num2) => num1 === num2 ? 1 : -1
